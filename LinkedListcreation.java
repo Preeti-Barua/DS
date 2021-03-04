8841d721c9d6ed4b7f6ade1b695d60a438b09e00
PREETI <40754414+Preeti-Barua@users.noreply.github.com>
2021-03-04 23:56:42 +0530

    LinkedList insertion in java

---
+++ LinkedListcreation.java
@@ -0,0 +1,73 @@
+public class LinkedListcreation {
+	
+    class Node{  
+        int data;  
+        Node next;  
+          
+        public Node(int data) {  
+            this.data = data;  
+            this.next = null;  
+        }  
+    }  
+   
+    //Represent the head and tail of the singly linked list  
+    public Node head = null;  
+    public Node tail = null;  
+      
+    //addNode() will add a new node to the list  
+    public void addNode(int data) {  
+        //Create a new node  
+        Node newNode = new Node(data);  
+          
+        //Checks if the list is empty  
+        if(head == null) {  
+            //If list is empty, both head and tail will point to new node  
+            head = newNode;  
+            tail = newNode;  
+        }  
+        else {  
+            //newNode will be added after tail such that tail's next will point to newNode  
+            tail.next = newNode;  
+            //newNode will become new tail of the list  
+            tail = newNode;  
+        }  
+    }  
+      
+    //display() will display all the nodes present in the list  
+    public void display() {  
+        //Node current will point to head  
+        Node current = head;  
+          
+        if(head == null) {  
+            System.out.println("List is empty");  
+            return;  
+        }  
+        System.out.println("Nodes of singly linked list: ");  
+        while(current != null) {  
+            //Prints each node by incrementing pointer  
+            System.out.print(current.data + " ");  
+            current = current.next;  
+        }  
+        System.out.println();  
+    }  
+		
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LinkedListcreation l=new LinkedListcreation();
+		l.addNode(3);
+		l.addNode(4);
+		l.addNode(5);
+		l.addNode(66);
+		l.addNode(376);
+		
+		l.display();
+
+	}
+
+}
